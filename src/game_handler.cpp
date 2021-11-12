15974c74751917d56cb75c00a7529b6029016028
Sawate <Sawate@deeplay.io>
2021-11-12 05:47:10 +0300

    [*] game_handler.h, game_handler.cpp: добавлены методы для получения и записи действий игроков, переработано использование figure, скорректированы комментарии

--- src/game_handler.cpp
+++ src/game_handler.cpp
@@ -2,7 +2,7 @@
 
 bool game_handler::is_correct_move(const coordinate& cord) const
 {
-	if (game_field_.get(cord) == figure::empty)
+	if (game_field_.get(cord).is_empty())
 	{
 		return true;
 	}
@@ -18,6 +18,7 @@ figure game_handler::is_game_finished() const
 		{
 			return game_field_.get({ i, 0 });
 		}
+
 		if (game_field_.get({ 0, i }) == game_field_.get({ 1, i })
 			&& game_field_.get({ 1, i }) == game_field_.get({ 2, i }))
 		{
@@ -33,73 +34,86 @@ figure game_handler::is_game_finished() const
 		return game_field_.get({ 1, 1 });
 	}
 
-	return figure::empty;
+	return figure::get_empty_figure();
 }
 
 figure game_handler::do_moves()
 {
-	while (number_of_moves_ < 9)
+	while (number_of_moves_ < max_number_of_moves)
 	{
 		const size_t player_number = number_of_moves_ % 2;
-		const auto current_figure = static_cast<const figure>(player_number);
+		const auto& current_player = players_[player_number];
+		const auto current_figure = get_current_players_figure();
 
-		std::cout << "Move #" << number_of_moves_ << ", "
-			<< players_[player_number]->get_name() << " move" << std::endl
-			<< "Game field: " << std::endl;
+		std::cout << "Move #" << number_of_moves_ << ", " << current_player->get_name()
+			<< " move" << std::endl << "Game field: " << std::endl;
 
 		game_field_.print_game_field_to_cmd();
 
-		const coordinate player_move = players_[player_number]->get_move(game_field_, number_of_moves_, current_figure);
-		if (!is_correct_move(player_move))
-		{
-			std::cout << "Incorrect move from player " + players_[player_number]->get_name()
-				<< std::endl << "Technical lose for this player." << std::endl;
-			return static_cast<const figure>(!player_number);
-		}
-		game_field_.set(player_move, current_figure);
+		const coordinate players_move = get_move_from_player(current_player, current_figure);
 
-		std::cout << "Player " << players_[player_number]->get_name()
-			<< " made move on [" << player_move.column << ", " << player_move.row << "]"
-			<< std::endl << std::endl << std::endl;
+		if (!make_players_move(current_player, players_move, current_figure))
+		{
+			return current_figure.get_opposite_figure();
+		}
 
 		const auto win_figure = is_game_finished();
-		if (win_figure != figure::empty)
+		if (!win_figure.is_empty())
 		{
-			std::cout << "Player " << players_[player_number]->get_name()
-				<< " won the game!!!!" << std::endl;
+			std::cout << "Player " << current_player->get_name()
+				<< " won the game!" << std::endl;
 			return win_figure;
 		}
 
 		number_of_moves_++;
 	}
-	return figure::empty;
+
+	return figure::get_empty_figure();
 }
 
 void game_handler::finish_round(const figure& fig) const
 {
 	game_field_.print_game_field_to_cmd();
-	if (fig == figure::empty)
+	if (fig.is_empty())
 	{
 		std::cout << "Game over! The game is a draw." << std::endl << std::endl;
 	}
 	else
 	{
-		std::cout << "Player " << get_players_number_by_enum(fig) << " won!" << std::endl << std::endl;
+		std::cout << "Player " << fig.get_int() << " won!" << std::endl << std::endl;
 	}
 }
 
-size_t game_handler::get_players_number_by_enum(const figure& fig)
+figure game_handler::get_current_players_figure() const
 {
-	switch (fig)
+	if (number_of_moves_ % 2 == 0)
 	{
-	case figure::cross:
-		return 0;
-	case figure::nought:
-		return 1;
-	case figure::empty:
-	default:
-		return 2;
+		return figure(figure_enum::cross);
 	}
+	return figure(figure_enum::nought);
+}
+
+coordinate game_handler::get_move_from_player(const std::shared_ptr<player>& player, const figure& players_figure) const
+{
+	return player->get_move(game_field_, number_of_moves_, players_figure);
+}
+
+bool game_handler::make_players_move(const std::shared_ptr<player>& player, const coordinate& players_move, const figure& players_figure)
+{
+	if (!is_correct_move(players_move))
+	{
+		std::cout << "Incorrect move from player " + player->get_name()
+			<< std::endl << "Technical lose for this player." << std::endl;
+		return false;
+	}
+
+	game_field_.set(players_move, players_figure);
+
+	std::cout << "Player " << player->get_name()
+		<< " made move on [" << players_move.column << ", " << players_move.row << "]"
+		<< std::endl << std::endl;
+
+	return true;
 }
 
 size_t game_handler::play_game()
@@ -109,16 +123,8 @@ size_t game_handler::play_game()
 		<< players_[1]->get_name() << std::endl;
 
 	const auto win_figure = do_moves();
+
 	finish_round(win_figure);
 
-	switch (win_figure)
-	{
-	case figure::cross:
-		return 0;
-	case figure::nought:
-		return 1;
-	case figure::empty:
-	default:
-		return 2;
-	}
+	return win_figure.get_int();
 }
--- src/game_handler.h
+++ src/game_handler.h
@@ -12,7 +12,7 @@ class game_handler
 {
 private:
 	/**
-	 * \brief Кколичество рядов
+	 * \brief Количество рядов
 	 */
 	static constexpr size_t rows = 3;
 
@@ -20,6 +20,11 @@ private:
 	 * \brief Количество столбцов
 	 */
 	static constexpr size_t columns = 3;
+
+	/**
+	 * \brief Максимальное количество ходов в игре
+	 */
+	static constexpr size_t max_number_of_moves = 9;
 
 	/**
 	 * \brief Массив игроков
@@ -32,7 +37,7 @@ private:
 	game_field game_field_;
 
 	/**
-	 * \brief Количество действий, совершённых в игре
+	 * \brief Количество действий, совершенных в игре
 	 */
 	size_t number_of_moves_ = 0;
 
@@ -41,19 +46,20 @@ private:
 	 * \param cord Координаты хода игрока
 	 * \return Может ли игрок сделать такой ход
 	 */
-	bool is_correct_move(const coordinate& cord) const;
+	[[nodiscard]] bool is_correct_move(const coordinate& cord) const;
 
 	/**
-	 * \brief Метод для проверки того, не зкончилась ли ещё игра
-	 * \return Фигуру выигравшего игрока, если победителей нет, то возвращает figure::empty
+	 * \brief Метод для проверки того, не закончилась ли ещё игра
+	 * \return Фигуру выигравшего игрока, если победителей нет
+	 * или игра не закончена, то возвращает пустую фигуру
 	 */
-	figure is_game_finished() const;
+	[[nodiscard]] figure is_game_finished() const;
 
 	/**
 	 * \brief Метод для обработки действий игроков
-	 * \return Фигура игрока, который победил, если победителей нет, то возвращает figure::empty
+	 * \return Фигура игрока, который победил, если победителей нет, то возвращает пустую фигуру
 	 */
-	figure do_moves();
+	[[nodiscard]] figure do_moves();
 
 	/**
 	 * \brief Метод для вывода информации в консоль о об окончании игры
@@ -62,11 +68,27 @@ private:
 	void finish_round(const figure& fig) const;
 
 	/**
-	 * \brief Вспомогательный метод для определения номера игрока по фигуре. Для empty результат = 0
-	 * \param fig Фигура игрока
-	 * \return Номер игрока
+	 * \brief Метод для получения фигуры игрока на текущем ходу
+	 * \return Фигура игрока для текущего хода (number_of_moves_)
 	 */
-	static size_t get_players_number_by_enum(const figure& fig);
+	[[nodiscard]] figure get_current_players_figure() const;
+
+	/**
+	 * \brief Метод для получения действия от игрока
+	 * \param player Игрок, от которого нужно получить действие
+	 * \param players_figure Фигура игрока
+	 * \return Координаты, по которым хочет походить игрок
+	 */
+	[[nodiscard]] coordinate get_move_from_player(const std::shared_ptr<player>& player, const figure& players_figure) const;
+
+	/**
+	 * \brief Метод для записи действия игрока
+	 * \param player Игрок, совершивший действие
+	 * \param players_move Действие, полученное от игрока
+	 * \param players_figure Фигура игрока
+	 * \return Получилось ли записать действие
+	 */
+	[[nodiscard]] bool make_players_move(const std::shared_ptr<player>& player, const coordinate& players_move, const figure& players_figure);
 
 public:
 	/**
@@ -84,5 +106,5 @@ public:
 	 * 1 - победил второй игрок
 	 * 2 - ничья
 	 */
-	size_t play_game();
+	[[nodiscard]] size_t play_game();
 };
